c90d2e99880e5859dc9c0139c07764f2132a4a01
Gabriel Santana <gabrielluizsantana.9@gmail.com>
2022-03-16 20:52:01 -0300

    Limpeza de  código

--- EF.Core.Multitenant/EF.Core.Multitenant/Data/ApplicationContext.cs
+++ EF.Core.Multitenant/EF.Core.Multitenant/Data/ApplicationContext.cs
@@ -24,18 +24,6 @@ namespace Multitenant.Infraestructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //estratégia 1
-            //modelBuilder.Entity<Person>().HasData(new Person { Id = 1, Name = "Person 1", TenantId = "tenant-1" },
-            //    new Person { Id = 2, Name = "Person 2", TenantId = "tenant-2" },
-            //    new Person { Id = 3, Name = "Person 3", TenantId = "tenant-3" });
-
-            //modelBuilder.Entity<Product>().HasData(new Product { Id = 1, Description = "Description 1", TenantId = "tenant-1" },
-            //    new Product { Id = 2, Description = "Description 2", TenantId = "tenant-2" },
-            //    new Product { Id = 3, Description = "Description 3", TenantId = "tenant-3" });
-
-            //modelBuilder.Entity<Person>().HasQueryFilter(x => x.TenantId == _tenant.TenantId);
-            //modelBuilder.Entity<Product>().HasQueryFilter(x => x.TenantId == _tenant.TenantId);
-
 
             modelBuilder.HasDefaultSchema(TenantData.TenantId);
 
